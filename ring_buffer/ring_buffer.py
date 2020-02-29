46a2e3179d270e776d62c5f0416d1eb379ab753c
Rashmi <poddar.rashmi@gmail.com>
2020-02-29 13:58:19 -0500

    Updated ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,40 +8,29 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        q = Queue()
         if self.capacity != self.storage.length:
-            if self.storage.length == 0:
-                self.storage.add_to_head(item)
-                self.current = item
-                q.enqueue(item)
-            else:
-                self.storage.add_to_tail(item)
-                self.current = item
-                q.enqueue(item)
+            self.storage.add_to_tail(item)
+            self.current = self.storage.tail 
         else:
-            print(self.current)
-            self.storage.remove_from_head()
-            self.storage.add_to_head(item)  
+            if self.current.next:
+                self.storage.delete(self.current.next)
+                self.current.insert_after(item)
+                self.storage.length += 1
+                self.current = self.current.next
+            else:
+                self.current = self.storage.head
+                self.storage.delete(self.current)
+                self.storage.add_to_head(item)
+                self.current = self.storage.head
         
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
-        # TODO: Your code here
-        print(self.storage.length)
-        if self.storage.length is None:
-            return list_buffer_contents
         current = self.storage.head
-        print(current.value)
         while current:
             list_buffer_contents.append(current.value)
-            if current.next: 
-                current = current.next
-            else: 
-                break
+            current = current.next
         return list_buffer_contents
-        
-    
 
 
 # ----------------Stretch Goal-------------------
@@ -58,12 +47,22 @@ class ArrayRingBuffer:
         pass
 
 
-rb = RingBuffer(5)
+# rb = RingBuffer(5)
+# rb.append('a')
+# rb.append('b')
+# rb.append('c')
+# rb.append('d')
+# rb.append('e')
+# rb.append('f')
 # print(rb.get())
-rb.append('a')
-rb.append('b')
-rb.append('c')
-rb.append('d')
-rb.append('e')
-print('the array is ', rb.get())
-rb.append('e')
+# rb.append('g')
+# print(rb.get())
+# rb.append('h')
+# print(rb.get())
+# rb.append('i')
+# rb.append('j')
+# rb.append('k')
+# print(rb.get())
+# for i in range(50):
+#     rb.append(i)
+#     print(rb.get())
